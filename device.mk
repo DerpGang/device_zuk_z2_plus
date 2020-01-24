143e5fd6a14e560092637e3a387b7f17fac53c74
Arseniy Graur <argraur@gmail.com>
2020-01-24 14:08:14 +0000

    z2_plus: Use Pixel offline charger animation
    Signed-off-by: Arseniy Graur <argraur@gmail.com>
    Change-Id: Ie0d25e37912fffd337731d4c5eb9e9ccf09a2696

--- device.mk
+++ device.mk
@@ -131,6 +131,11 @@ PRODUCT_PACKAGES += \
     services-ext \
     libcnefeatureconfig
 
+# Charger images
+PRODUCT_PACKAGES += \
+    charger_res_images \
+    product_charger_res_images
+
 # Display
 PRODUCT_PACKAGES += \
     copybit.msm8996 \
