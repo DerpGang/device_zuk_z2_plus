6ecaf41a6ebb71f5e6c41f7f2cee7aea287a28d9
ZyxxeZ <ZyxxeZ@users.noreply.github.com>
2020-11-07 14:49:58 +0000

    z2_plus: build ipacm and tetheroffload
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- hidl.mk
+++ hidl.mk
@@ -104,6 +104,18 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service
 
+# Ipacm
+PRODUCT_PACKAGES += \
+    ipacm \
+    IPACM_cfg.xml \
+    libipanat \
+    liboffloadhal
+
+# Tetheroffload
+PRODUCT_PACKAGES += \
+    android.hardware.tetheroffload.config@1.0 \
+    android.hardware.tetheroffload.control@1.0
+
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl:64 \
