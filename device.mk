f79a425ff90c6867c21958f44820494cedb19dc6
Dmitrii <bankersenator@gmail.com>
2020-11-28 17:09:46 +0000

    z2_plus: add vndk 29 libprotobuf prebuilts
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- device.mk
+++ device.mk
@@ -361,6 +361,11 @@ PRODUCT_PACKAGES += \
     libprotobuf-cpp-full-vendorcompat \
     libprotobuf-cpp-lite-vendorcompat
 
+# VNDK 29 prebuilts
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full-v29.so \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite-v29.so
+
 # Wifi
 PRODUCT_PACKAGES += \
     libqsap_sdk \
