19868ff2db23896cfa14db77380b7a59da5fab5c
Sujin Panicker <spanic@codeaurora.org>
2021-01-28 11:35:52 +0400

    z2_plus: Add libnbaio to PRODUCT_PACKAGES libnbaio is not installed for system/lib/ for HY11 builds.
    Change-Id: Ie3c913542a06f2096841b5821baea60b0077a084
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- device.mk
+++ device.mk
@@ -95,7 +95,8 @@ PRODUCT_PACKAGES += \
     libqcomvisualizer \
     libqcomvoiceprocessing \
     libvolumelistener \
-    libnl
+    libnl \
+    libnbaio
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
