3b0a9a147a804f36ee9309c275cf2fb470b39288
Dmitrii <bankersenator@gmail.com>
2021-03-18 09:27:06 +0000

    z2_plus: kang new sensors from LA.UM.9.6.2.r1-03600-89xx.0
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- hidl.mk
+++ hidl.mk
@@ -89,7 +89,9 @@ PRODUCT_PACKAGES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service
+    android.hardware.sensors@1.0-service \
+    android.hardware.sensors@2.0 \
+    android.hardware.sensors@2.1
 
 # USB HAL
 PRODUCT_PACKAGES += \
