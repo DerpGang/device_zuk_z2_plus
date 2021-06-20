836e99012dc5732d1c81e7783eba40ec66fa4b3a
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-06-20 02:26:17 +0200

    z2_plus: Stop building vendor.qti.hardware.perf lib
    * We dropped QTI perf stack
    
    Change-Id: Ie71344aa05888adb7ad76ad66693547fc50a0025
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- hidl.mk
+++ hidl.mk
@@ -92,7 +92,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
     android.hardware.power.stats@1.0-service.mock \
-    vendor.qti.hardware.perf@2.0.vendor
 
 # RenderScript HAL
 PRODUCT_PACKAGES += \
