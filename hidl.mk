921feddce33936a5ddb1cf921c2d652726ae7f64
Subhajeet Muhuri <subhajeet.muhuri@aosip.dev>
2021-07-22 22:44:02 +0000

    z2_plus: add modified powerhint.json
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- hidl.mk
+++ hidl.mk
@@ -93,6 +93,9 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service.zuk_msm8996-libperfmgr \
     android.hardware.power.stats@1.0-service.mock \
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json \
+
 PRODUCT_SOONG_NAMESPACES += \
     hardware/google/interfaces \
     hardware/google/pixel
