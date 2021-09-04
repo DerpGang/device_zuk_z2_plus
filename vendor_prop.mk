32a7f08240902107abdca4de0de9e6d745009251
Dmitrii <bankersenator@gmail.com>
2021-09-04 15:40:39 +0000

    z2_plus: add vendor prefix to qcom.bluetooth.soc
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -35,7 +35,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
     bt.max.hfpclient.connections=1 \
-    qcom.bluetooth.soc=rome \
+    vendor.qcom.bluetooth.soc=rome \
     ro.bluetooth.emb_wp_mode=true \
     ro.vendor.bt.bdaddr_path=/mnt/vendor/persist/bluetooth/bt_mac \
     ro.bluetooth.wipower=true
