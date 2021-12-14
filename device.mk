81fbb6f6d669e324e5f2d92881c7f632b746d15a
Pranav Temkar <pranavtemkar@gmail.com>
2021-12-14 09:33:45 +0000

    z2_plus: Nuke Lineage Overlays
     - These are LineageOS specific & not needed anymore
    
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>

--- device.mk
+++ device.mk
@@ -25,8 +25,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 # RRO (Runtime Resource Overlay)
 TARGET_USES_RRO := true
