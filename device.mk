2254d11f21c18b9fed62c33a603c60b48dd301c7
Roberto Sartori <roberto.sartori.android@gmail.com>
2022-05-17 14:12:42 +0400

    z2_plus: disable compressed APEX Compressed APEX has been introduced in Android 12, and they are enabled by default inheriting updatable_apex.mk. Unfortunately, when the device is encrypted, they are causing bootloops on msm8937. As the only advantage is some space gain on disk, we can simply disable them: keep the variable immediatly after including the updatable_apex.mk to avoid being overwritten.
    Signed-off-by: Roberto Sartori <roberto.sartori.android@gmail.com>
    Change-Id: I410bd4ad8d589971339dddab709c5b40d9c9a15c
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- device.mk
+++ device.mk
@@ -33,6 +33,9 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay-lineage/lineage-sdk
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/packages/apps/CarrierConfig
 
+# Apex
+PRODUCT_COMPRESSED_APEX := false
+
 # Permissions
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml \
