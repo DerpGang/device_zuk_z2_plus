15c79f1044a473085b8fb9865e23e4d8b86de0c8
Dmitrii <bankersenator@gmail.com>
2022-08-18 03:01:41 +0000

    z2_plus: remove encryption
    FDE was removed in a13
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- device.mk
+++ device.mk
@@ -261,10 +261,6 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl.recovery \
     android.hardware.health@2.1-service
 
-# HW crypto
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
-
 # IPC router config
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
