39ef3d190c0584220cc89022a1591743794d2888
Josh Wu <joshwu@google.com>
2022-08-23 19:55:40 +0400

    z2_plus: Switch Bt Audio to AIDL Test: m Bug: 203490261 Change-Id: I4b8ac40d5ad410b89eb6a0573d740d86471ecfc7 Merged-In: I4b8ac40d5ad410b89eb6a0573d740d86471ecfc7 Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- device.mk
+++ device.mk
@@ -102,6 +102,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@6.0 \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.soundtrigger@2.1-impl \
+    android.hardware.bluetooth.audio-impl \
     android.hardware.broadcastradio@1.0-impl \
     audiod \
     audio.primary.msm8996 \
